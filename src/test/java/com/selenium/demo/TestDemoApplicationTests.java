7c75e8b66a4b3eba0f3ecdcf45de8c78739142db
giteauser <gitea@fake.local>
2021-06-15 13:37:08 +0530

    first commit

---
+++ src/test/java/com/selenium/demo/TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selenium.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
