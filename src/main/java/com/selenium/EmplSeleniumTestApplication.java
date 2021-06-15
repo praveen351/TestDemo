c6220f26caf25af631bfe52fef9aff1a0029a727
giteauser <gitea@fake.local>
2021-06-15 16:54:32 +0530

    second commit

---
+++ src/main/java/com/selenium/EmplSeleniumTestApplication.java
@@ -0,0 +1,12 @@
+package com.selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplSeleniumTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplSeleniumTestApplication.class, args);
+	}
+}
